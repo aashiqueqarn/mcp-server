ae3d6eb9e4d04247cc29a689ea8fae5c29e843ca
Aashique Karn <aashiquekarn@gmail.com>
2026-02-11 11:21:22 +0530

    master | initial commit | adding mcp server codes

---
+++ main.py
@@ -0,0 +1,31 @@
+import asyncio
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+from langchain_mcp_adapters.tools import load_mcp_tools
+from langgraph.prebuilt import create_react_agent
+from mcp import ClientSession, StdioServerParameters
+from mcp.client.stdio import stdio_client
+from langchain_core.messages import HumanMessage
+
+load_dotenv()
+
+llm = ChatOpenAI(temperature=0)
+
+stdio_server_params = StdioServerParameters(
+    command="python",
+    args=["/Users/aashiquekarn/Desktop/Personal/MCP_Servers/mcp-server/servers/math_server.py"],
+)
+
+async def main():
+    async with stdio_client(stdio_server_params) as (read, write):
+        async with ClientSession(read_stream=read, write_stream=write) as session:
+            await session.initialize()
+            print('Session initialized')
+            tools = await load_mcp_tools(session)
+            agent = create_react_agent(llm, tools)
+            result = await agent.ainvoke({"messages": [HumanMessage(content="What is 2 + 2 * 5?")]})
+            print(result["messages"][-1].content)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
